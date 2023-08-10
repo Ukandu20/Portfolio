974ce420755910731c8320b3ef36ad9ee98e5b11
O_Ukandu <100975029+Ukandu20@users.noreply.github.com>
2023-08-10 10:35:20 -0400

    modified:   contact-form.php 	modified:   contact.html 	deleted:    js/contact.js

--- contact-form.php
+++ contact-form.php
@@ -2,13 +2,15 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $recipient = "oukandu2000@gmail.com"; // Your email address
     $subject = $_POST["subject"];
-    $name = $_POST["name"];
+    $fname = $_POST["first_name"];
+    $lname = $_POST["last_name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
     
-    $headers = "From: $name <$email>";
+    $headers = "From:" .$fname. "<".$email.">";
     
     mail($recipient, $subject, $message, $headers);
+    or die("Error!");
     
     echo "Message sent successfully!";
 }
